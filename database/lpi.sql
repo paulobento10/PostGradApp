2a54ad179587b09620544746a46dbbe406b8b58e
pfsb <33959@ufp.edu.pt>
2018-06-16 16:55:24 +0100

    Post grad app

---
+++ database/lpi.sql
@@ -0,0 +1,197 @@
+-- MySQL dump 10.13  Distrib 5.6.33, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: lpi
+-- ------------------------------------------------------
+-- Server version	5.6.33-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `evento`
+--
+
+DROP TABLE IF EXISTS `evento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evento` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `sala` varchar(255) DEFAULT NULL,
+  `data` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fim` time NOT NULL,
+  `estado` int(11) NOT NULL,
+  `tipo` text,
+  `contacto` text,
+  `notas` text,
+  `cacifo_digital_id` varchar(255) DEFAULT NULL,
+  `skype_id` varchar(255) DEFAULT NULL,
+  `utilizador_id` int(11) NOT NULL,
+  `convidado_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `utilizador_id` (`utilizador_id`),
+  KEY `FK_EventoProjetoID` (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evento`
+--
+
+LOCK TABLES `evento` WRITE;
+/*!40000 ALTER TABLE `evento` DISABLE KEYS */;
+INSERT INTO `evento` VALUES (6,'alteracao do projeto','A definir','2018-04-02','09:00:00','13:00:00',1,'skype','912664700','urgente','','pedro',50,50,9),(7,'alteracao do projeto','A definir','2018-04-05','09:00:00','13:00:00',1,'skype','912664700','urgente','','pedro',50,50,9);
+/*!40000 ALTER TABLE `evento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `utilizador_id` int(11) NOT NULL,
+  `mongoID` text NOT NULL,
+  `receptor_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `files`
+--
+
+LOCK TABLES `files` WRITE;
+/*!40000 ALTER TABLE `files` DISABLE KEYS */;
+/*!40000 ALTER TABLE `files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projeto`
+--
+
+DROP TABLE IF EXISTS `projeto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projeto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` text NOT NULL,
+  `area` text NOT NULL,
+  `tema` text NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `utilizador_id` int(11) NOT NULL,
+  `orientador_id` int(11) NOT NULL,
+  `coorientador_id` int(11) DEFAULT NULL,
+  `projeto_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `projeto_id` (`projeto_id`),
+  KEY `utilizador_id` (`utilizador_id`),
+  KEY `FK_ProjetoOrientador` (`orientador_id`),
+  KEY `FK_ProjetoCoorientador` (`coorientador_id`),
+  CONSTRAINT `projeto_ibfk_1` FOREIGN KEY (`utilizador_id`) REFERENCES `utilizador` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projeto`
+--
+
+LOCK TABLES `projeto` WRITE;
+/*!40000 ALTER TABLE `projeto` DISABLE KEYS */;
+INSERT INTO `projeto` VALUES (9,'teste','Engenharia Informatica','elearning',0,'2018-04-02',50,50,NULL,NULL);
+/*!40000 ALTER TABLE `projeto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resultado`
+--
+
+DROP TABLE IF EXISTS `resultado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `resultado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `classificacao` text NOT NULL,
+  `data` date NOT NULL,
+  `projeto_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `projeto_id` (`projeto_id`),
+  CONSTRAINT `resultado_ibfk_1` FOREIGN KEY (`projeto_id`) REFERENCES `projeto` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resultado`
+--
+
+LOCK TABLES `resultado` WRITE;
+/*!40000 ALTER TABLE `resultado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `resultado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilizador`
+--
+
+DROP TABLE IF EXISTS `utilizador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilizador` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `perfil_id` varchar(12) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_pessoal` varchar(255) DEFAULT NULL,
+  `is_email_valid` int(11) NOT NULL,
+  `is_email_pessoal_valid` int(11) DEFAULT NULL,
+  `password` text NOT NULL,
+  `tipo` int(11) NOT NULL,
+  `formacao_avancada` int(11) NOT NULL,
+  `foto` blob,
+  `foto_tipo` text,
+  `contacto` text NOT NULL,
+  `skype_id` text,
+  `cacifo_digital_id` text,
+  `horario_livre` text,
+  `notificacao` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `perfil_id` (`perfil_id`),
+  UNIQUE KEY `email_pessoal` (`email_pessoal`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilizador`
+--
+
+LOCK TABLES `utilizador` WRITE;
+/*!40000 ALTER TABLE `utilizador` DISABLE KEYS */;
+INSERT INTO `utilizador` VALUES (53,'009699','Prof New','labproaulas@gmail.com',NULL,1,0,'$2y$11$UXP9yUCLofTX2IzUyn0jhOvqjI/fEHvU1u/twpsMJlt9tTgcbC57O',0,0,'','','123456789','1234','1234',NULL,1),(54,'114584','Paulo Bento','33959@ufp.edu.pt','',1,0,'$2y$11$NXdloIyfV0IFByAyM1tbKeHeFVsnCH5KF8T7YRaLktrc8YTWsXgj.',1,0,'','','987654321','123','123',NULL,1);
+/*!40000 ALTER TABLE `utilizador` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-03 13:07:46
